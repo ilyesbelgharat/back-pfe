b249989651e33571013b6d711417d8e9153615e9
ilyesbelgharat <53319470+ilyesbelgharat@users.noreply.github.com>
2019-07-26 09:01:34 -0400

    Update DatabaseConfig.java

--- src/main/java/org/sid/DatabaseConfig.java
+++ src/main/java/org/sid/DatabaseConfig.java
@@ -1,6 +1,6 @@
-package com.example.demo;
+package org.sid;
 
-import com.zaxxer.hikari.*;
+import org.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
