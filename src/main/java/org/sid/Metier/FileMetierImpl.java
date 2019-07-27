ac8a429b4fc6f8affd03f357345fc734b09313bd
ilyesbelgharat <53319470+ilyesbelgharat@users.noreply.github.com>
2019-07-27 08:44:22 -0400

    Update FileMetierImpl.java

--- src/main/java/org/sid/Metier/FileMetierImpl.java
+++ src/main/java/org/sid/Metier/FileMetierImpl.java
@@ -55,7 +55,7 @@ private ProjetRepository projetRepository;
             storageService.store(file);
             files.add(file.getOriginalFilename());
             //c'est aussi prouvesoir
-            s="http://localhost:8080/files/"+file.getOriginalFilename().replace('.','/').replace(' ','-');
+            s="http://ilyesapprisk.herokuapp.com/files/"+file.getOriginalFilename().replace('.','/').replace(' ','-');
             //s=file.getOriginalFilename();
             f.setPath(s);
 f.setFileName(file.getOriginalFilename());
