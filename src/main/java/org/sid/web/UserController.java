b6a4674a6f7cf3a355ff6f841ff1f0a7a7de77d5
ilyesbelgharat <53319470+ilyesbelgharat@users.noreply.github.com>
2019-07-28 00:00:53 -0400

    Update UserController.java

--- src/main/java/org/sid/web/UserController.java
+++ src/main/java/org/sid/web/UserController.java
@@ -32,18 +32,11 @@ public class UserController {
    private AppRoleRepository appRoleRepository;
  @RequestMapping(value="/register1",method= RequestMethod.POST)
     public int register1(){
-          File temp;
-        try
-        {
-            temp = File.createTempFile("myTempFile", ".txt");
-            boolean exists = temp.exists();
-            System.out.println("Temp file exists : " + exists);
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        System.out.println(this.getClass().getProtectionDomain().getCodeSource().getLocation());
-System.out.println(System.getProperty("user.dir"));
+          File tmpDir = new File("/var/tmp");
+        boolean exists = tmpDir.exists();
+
+        System.out.println("Temp file exists : " + exists);
+       
 
                     return  1;
             }
