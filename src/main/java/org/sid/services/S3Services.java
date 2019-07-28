8cfb8f7e929954039fe3be6f6e643c8419f4415d
ilyesbelgharat <53319470+ilyesbelgharat@users.noreply.github.com>
2019-07-28 18:40:54 +0200

    Add files via upload

---
+++ src/main/java/org/sid/services/S3Services.java
@@ -0,0 +1,8 @@
+package org.sid.services;
+import java.io.ByteArrayOutputStream;
+
+import org.springframework.web.multipart.MultipartFile;
+public interface S3Services {
+    public ByteArrayOutputStream downloadFile(String keyName);
+    public void uploadFile(String keyName, MultipartFile file);
+}
