13afaa7101edab5ba35780fee1e3081d30ad9f3c
ilyesbelgharat <53319470+ilyesbelgharat@users.noreply.github.com>
2019-07-30 12:15:45 -0400

    Update UserController.java

--- src/main/java/org/sid/web/UserController.java
+++ src/main/java/org/sid/web/UserController.java
@@ -42,9 +42,11 @@ public class UserController {
               List<AppRole> appRoles=appRoleRepository.findAll();
         System.out.println(appRoles);
        for (int i=0;i<appRoles.size();i++){
+                           if(appRoles.get(i).getRoleName()!=null ){
+
                 if(appRoles.get(i).getRoleName().equals("USER") ){
-               //     appRoleRepository.delete(appRoles.get(i));
-             }
+                    appRoleRepository.delete(appRoles.get(i));
+             }}
         }
 
                     return  1;
